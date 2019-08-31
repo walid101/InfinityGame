00beed491162092c16b3e8592b4c606d15ee91f5
walid101 <45158255+walid101@users.noreply.github.com>
2019-08-31 08:06:56 -0400

    Documentation for GameScreen

--- core/src/Screens/GameScreen.java
+++ core/src/Screens/GameScreen.java
@@ -18,26 +18,35 @@ import ScreenMod.mouseControl;
 
 public class GameScreen implements Screen
 {
-    Animation<TextureRegion> animation;
-    Dialogue converse;
-    Dialogue converse2;
-    boolean stop = false;
-    float elapsed;
-    Texture back;
-    IG thisGame;
-    Music music2;
-    Texture click;
-    //These are the x, and y for click, also
-    //Height and width for click
-    int clickX = 208;
-    int clickY = 170;
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    int vel = 2;
-    int clickWidth = 63;
-    int clickHeight = 60;
 
+    Animation<TextureRegion> animation;//This stores GIF screens in the future
+    Dialogue converse; // first character to user interaction by dialogue
+    Dialogue converse2; // second character to user interaction by dialogue
+    boolean stop = false; //
+    float elapsed;
+
+    Texture back; // the background image of the GameScreen
+    IG thisGame; // taken from the parent class IG game to use the draw method
+    Music music2; // the background music for GameScreen
+    Texture click; //the rotating circles that will be the click index for user
+
+    //These are the x, and y for click, also
+    //Height and width for click :::::::::::::::::::::::::::;
+        int clickX = 208;
+        int clickY = 170;
+        //i, j, k are the relative locations for each circle
+        //this is because eventually when the user hovers over
+        //the circles will have different speeds
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        //this is what adds to the i,j, k (circle position relative to the y-axis)
+        //causing it to act like a speed scalar
+        int vel = 2;
+        //these are the set circle width and height for the click index
+        int clickWidth = 63;
+        int clickHeight = 60;
+    //::::::::::::::::::::::::::::::
     Texture clickCen;
     int click2X = 400;
     int click2Y = 240;
@@ -49,7 +58,7 @@ public class GameScreen implements Screen
     mouseControl control;//includes methods to help decode
                          //user input, go to the mouseControl
                          //class for more information.
-    ArrayList<String> des1;
+
 
     //These are the descriptions presented to user
     //when they hover over the rotating circles
@@ -69,24 +78,26 @@ public class GameScreen implements Screen
     public GameScreen(IG game)
     {
 
-        thisGame = game;
-        back = new Texture("backGame1.jpg");
-        control = new mouseControl(thisGame);
-        click = new Texture("click.png");
-        reg = new TextureRegion(click);
+        thisGame = game;//instantiating this game by passing it the main game from menu screen
+        back = new Texture("backGame1.jpg");//instantiating background image
+        control = new mouseControl(thisGame);//instantiating mouseControl class by passing it thisgame
+        click = new Texture("click.png");//instantiating the click class
+        reg = new TextureRegion(click);//encapsulating the click texture with a region to make it spin
 
-        clickCen = new Texture("click.png");
-        centReg = new TextureRegion(clickCen);
+        clickCen = new Texture("click.png");//instantiating the center click index
+        centReg = new TextureRegion(clickCen);//encapsulating center click index
 
-        clickBar = new Texture("click.png");
-        barReg = new TextureRegion(clickBar);
-        music2 = Gdx.audio.newMusic(Gdx.files.internal("Scene2Music.mp3"));//the music here needs to be changed,
-        //we will add the music in later.
-
+        clickBar = new Texture("click.png");//instantiating bar click index
+        barReg = new TextureRegion(clickBar);//encapsulating bar click texture
+        music2 = Gdx.audio.newMusic(Gdx.files.internal("Scene2Music.mp3"));//setting
+                                                                                 //background music
+        //here we are spiliting the gif into an arrayList of textures with a given delta time in between
         animation = GifDecoder.loadGIFAnimation(Animation.PlayMode.LOOP, Gdx.files.internal("backgroundRain.gif").read());
-
+        //giving the converse instances dialogue to say
         converse = new Dialogue("Ugh..............", "Where am I..........", "..Ugh My head...", "SONNY:", "SonnyIcon.png", 3);//Last one always goes first
         converse2 = new Dialogue("the last thing I remember was that doctor.......", "....who was he?....and why did he have the governmential signet?...", ".........", "SONNY:","SonnyIcon.png", 4);
+        //This is what will be printed on the screen when the user hovers over the click instaances, based on which
+        //they are hovering on top of
         log1 = new Dialogue("This is the store, " , "here you can buy new armaments", "for your characters, PRESS TO ENTER", "STORE", 3);
         log2 = new Dialogue("This is the NEXT WORLD", "please finish the current challenges first", " and then move forward, PRESS TO ENTER", "NEW WORLD", 3);
         log3 = new Dialogue("This is the Local bar", "Wait a minute, did you hear that crash?", "better check it out, PRESS TO ENTER", "first mission", 3 );
@@ -95,46 +106,57 @@ public class GameScreen implements Screen
     @Override
     public void show()
     {
-        music2.play();
+        music2.play();//playing background music
     }
 
     @Override
     public void render(float delta) {
+        //Each time the previous screen needs to be cleared, otherwise it is Extremely inefficient
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        thisGame.batch.begin();
-        thisGame.batch.enableBlending();
-        elapsed += Gdx.graphics.getDeltaTime();
-
+        thisGame.batch.begin();//starting the batch that we will draw on
+        thisGame.batch.enableBlending();//allows for clear background images
+        elapsed += Gdx.graphics.getDeltaTime();//getting time between frames
+        //controlling when to stop the music (like if the user selects this from the options menu)
         if (music2.isPlaying() == false && stop == false) {
             music2.play();
         }
+        //drawing the background, origin at 0,0 length and width fo the screen
         thisGame.batch.draw(back, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        //getting the deltaTime here
         thisGame.batch.draw(animation.getKeyFrame(elapsed), 20.0f, 20.0f);
+        //adding to the i,j,k positions to make the clicks spin
         i += vel;
         j += vel;
         k += vel;
+        //drawing the first click index (shop)
         thisGame.batch.draw(reg, clickX, clickY, clickWidth / 2, clickHeight / 2, clickWidth, clickHeight, 1, 1, i, true);
         //LOCATION TEST PURPOSES
-/**
- if(control.isHovering(Gdx.graphics.getWidth(), 0, Gdx.graphics.getHeight(), 0))
- {
- System.out.println("" + Gdx.input.getX() + " , " + Gdx.input.getY());
- }
- **/
+        /**
+         if(control.isHovering(Gdx.graphics.getWidth(), 0, Gdx.graphics.getHeight(), 0))
+         {
+         System.out.println("" + Gdx.input.getX() + " , " + Gdx.input.getY());
+         }
+         **/
+        //seeing if user is hovering over the shopClick index
         if (control.isHovering(clickX + clickWidth, clickX, clickY + clickHeight, clickY)) {
             log1.drawStaticDialogue(thisGame.batch, clickX, click2Y, Gdx.graphics.getWidth()/6);
+            //the index will spin faster to indicate that the user is hovering over it
             i+=vel;
+            //if user decides to click while also hovering over the shopClick index then the user
+            //enters the store
             if (control.isClicked()) {
                 System.out.println("Enter Store");
-                music2.pause();
-                thisGame.setScreen(new Store(thisGame));
-                stop = true;
+                music2.pause();//this is here just for rest
+                thisGame.setScreen(new Store(thisGame));//enters the store screen
+                stop = true;//stops the music
             }
         }
-
+        //drawing the centerClick index (if clicked sends user to new worls if they completed their quests)
         thisGame.batch.draw(centReg, click2X, click2Y, clickWidth / 2, clickHeight / 2, clickWidth, clickHeight, 1, 1, j, true);
+        //the mouse is hovering over the center region then continue::
         if (control.isHovering(click2X + clickWidth, click2X, click2Y + clickHeight, click2Y)) {
+            //making the index spin faster as to indicate that the user has hovered over it
             j+=vel;
             log2.drawStaticDialogue(thisGame.batch, click2X, click2Y, 2);
             if (control.isClicked()) {
